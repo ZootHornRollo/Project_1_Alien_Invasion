13f95aca75ec7f9b96a148c7a10a33c58656444c
ZootHornRollo <john.harris3@gmail.com>
2021-02-28 12:07:19 +0000

    Added the Bullet class and bullet functionality

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,8 @@
 import sys
-
 import pygame
-
 from settings import Settings
-
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -22,6 +20,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         #  Set background colour
         self.bg_colour = self.settings.bg_colour
@@ -31,6 +30,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -52,6 +52,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -59,10 +61,17 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_colour)
         self.ship.bliteme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Make the most recently drawn screen visible
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -3,6 +3,11 @@ class Settings:
 
     def __init__(self):
         """Initialise game settings."""
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
         # Screen settings
         self.screen_width = 1440
         self.screen_height = 900
